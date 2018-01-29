7809d01cb4a90b4ed9130e9a918f6d38f8b673b1
dprice80 <dprice80@gmail.com>
2018-01-29 14:58:41 +0000

    Reorganised queryfs properties

--- queryfs_example.m
+++ queryfs_example.m
@@ -1,10 +1,5 @@
 clear q
-
-q = query_files_obj('example_filesystem/id_info.csv');
-
-q.addsearchpath('example1','example_filesystem/<STUDYID>/file_<ID1>_*.mat')
-q.addsearchpath('example2','example_filesystem/<STUDYID>/file_<ID2>_*.mat')
-
-q.combine('ID1','ID2')
-
+q = queryfs('example_filesystem/id_info.csv');
+q.addsearchpath('example1','example_filesystem/<STUDYID>/file_<ID1|ID2>_*.mat');
 q.checkfiles
+
